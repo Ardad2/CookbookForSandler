e5216214d04ef0338eb13bd8ffcc3cd90598686c
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-12-07 15:35:35 -0700

    remove firebase config

--- firebase.js
+++ firebase.js
@@ -8,13 +8,6 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAUuZUgvlmMYBOl_Kg43Eb2sHnEJp4_us4",
-  authDomain: "sandler-cookbook.firebaseapp.com",
-  projectId: "sandler-cookbook",
-  storageBucket: "sandler-cookbook.appspot.com",
-  messagingSenderId: "519878306197", 
-  appId: "1:519878306197:web:4fe836d84e5caae16dae9c",
-  measurementId: "G-PF1Q3WHM3P"
 };
 
 const app = initializeApp(firebaseConfig);
